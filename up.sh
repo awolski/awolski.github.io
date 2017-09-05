1ea20d0ec7c8bba8f0fae8f0bf1330dc93b2b58e
Anthony Wolski <anthony.wolski@apexdatasolutions.net>
2017-09-05 06:23:01 +0100

    Add reducing email post

--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm --label=jekyll --volume=$PWD:/srv/jekyll -it -p 4000:4000 jekyll/jekyll:3.5 jekyll build
+docker run -d --rm --label=jekyll --volume=$PWD:/srv/jekyll -p 4000:4000 jekyll/jekyll:3.5 jekyll serve
