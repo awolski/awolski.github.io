6e24343d5609cb8ecaf3a535eddca4aceac0b578
Anthony Wolski <anthony.wolski@gmail.com>
2018-03-23 09:53:32 +0000

    Fix docker run command

--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -d --rm --label=jekyll --volume=$PWD:/srv/jekyll -p 4000:4000 jekyll/jekyll:3.5 jekyll serve
+docker run -d --rm --name jekyll --volume=$PWD:/srv/jekyll -p 4000:4000 jekyll/jekyll:3.5 jekyll serve
